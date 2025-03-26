8a8175389908aaef7e5cad6cbc5e0ee2faf7becd
Vincentc233 <VIncentc270@nycstudents.net>
2025-03-26 12:34:39 -0400

    Update Level.java

--- Level.java
+++ Level.java
@@ -1,1 +1,24 @@
+public class Level
+{
+    private boolean goal;
+    private int points;
+    private int sum;
 
+    public Level(boolean g, int p){
+        goal = g;
+        points = p;
+    }
+    /** Returns true if the player reached the goal on this level and returns false otherwise */
+    public boolean goalReached()
+    {
+        return goal;
+    }
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    {
+        if(goal) sum+= points;
+        return sum;
+    }
+
+// There may be instance variables, constructors, and methods that are not shown.
+}
